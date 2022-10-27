ed5d9965f12e3416566f14322c2f1f0bb26a8a9f
Bartosz <bartekg001@gmail.com>
2022-10-27 16:13:26 +0200

    Change calculate_volume to static and remove class constructor.

--- Lista_2/z2.cpp
+++ Lista_2/z2.cpp
@@ -1,45 +1,34 @@
 #include <iostream>
-#include <string>
 
 namespace cpplab
 {
-    template<typename T>
+    template<typename T, T _edge_length, int _dimensions>
     class hypercube
     {
         public: 
-        constexpr hypercube(T length, int dimensions)
+        static constexpr T calculate_volume()
         {
-            _edge_length = length;
-            _dimensions = dimensions;
-        }
-        constexpr T calculate_volume() const
-        {
-            if (_dimensions < 1)
+            if (_dimensions < 0)
                 return -1;
-            T volume = 1;
+            T volume = _dimensions == 0 ? 0 : 1;
             for (int i = 0; i < _dimensions; ++i)
                 volume *= _edge_length;
             return volume;
         }
-        private:
-        T _edge_length;
-        int _dimensions;
-        
     };
 }
 
 int main()
 {
-    constexpr cpplab::hypercube cube_1(10, 3);
-    std::cout << cube_1.calculate_volume() << "\n";
+    std::cout << cpplab::hypercube<int, 10, 3>::calculate_volume() << "\n";
     
-    static_assert(cube_1.calculate_volume() == 1000, "Nie uruchamia sie w trakcie kompilacji.");
+    static_assert(cpplab::hypercube<int, 10, 3>::calculate_volume() == 1000, "Nie uruchamia sie w trakcie kompilacji.");
 
-    constexpr cpplab::hypercube cube_2(14.86, 7);
-    std::cout << cube_2.calculate_volume() << "\n";
+    std::cout << cpplab::hypercube<double, 14.86, 7>::calculate_volume() << "\n";
 
-    constexpr cpplab::hypercube cube_3(64, -4);
-    std::cout << cube_3.calculate_volume() << "\n";
+    std::cout << cpplab::hypercube<int, 4, 0>::calculate_volume() << "\n";
+
+    std::cout << cpplab::hypercube<float, 64.9f, -4>::calculate_volume() << "\n";
 
     return 0;
 }
