85472e2f09d2175aebf9117c32f68819f3ddd47b
Bartosz <bartekg001@gmail.com>
2022-10-29 22:53:05 +0200

    Task 3 wip.

--- Lista_2/z3.cpp
+++ Lista_2/z3.cpp
@@ -2,11 +2,15 @@
 
 namespace cpplab
 {
-
+    template<typename T>
+    auto add_total(T *arg)
+    {
+        std::cout << sizeof(arg);
+    }
 }
 
 int main()
 {
-    
+    cpplab::add_total(1, 1.0f, 1.0);
     
 }
