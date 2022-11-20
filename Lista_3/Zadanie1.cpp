c85be1b673840324623dce74cf8958fef4c63986
Bartosz <bartekg001@gmail.com>
2022-11-20 14:25:36 +0100

    Pixel class ostream operator overload.

--- Lista_3/Zadanie1.cpp
+++ Lista_3/Zadanie1.cpp
@@ -22,6 +22,11 @@ namespace cpplab
             this->g = 0;
             this->b = 0;
         }
+        friend std::ostream &operator<<(std::ostream &os, const Pixel &pixel)
+        {
+            os << "(" << pixel.r << ", " << pixel.g << ", " << pixel.b << ")";
+            return os;
+        }
     };
 
     template <class T>
