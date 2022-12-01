8df3d60e1a77d447dddbbfb2ba0166156b7d7bda
Bartosz <bartekg001@gmail.com>
2022-12-01 21:07:03 +0100

    Implement basic functionality.

--- Lista_5/z1.cpp
+++ Lista_5/z1.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <string>
+
 namespace cpplab
 {
     int get_id()
     {
+        thread_local int thread_id = -1;
+        return thread_id;
     }
     void print(std::string const &text)
     {
+        std::cout << get_id() << " " << text << "\n";
     }
 }
 
