12eb9bbce96b47472f56fbb7d179c22304c27b62
Bartosz <bartekg001@gmail.com>
2022-12-15 16:18:30 +0100

    Add functions from previous task to assign and print thread id.

--- Lista_5/z2.cpp
+++ Lista_5/z2.cpp
@@ -1,5 +1,21 @@
 namespace cpplab
 {
+    int get_id()
+    {
+        static int global_id = 0;
+        static std::mutex mutex;
+        const std::lock_guard<std::mutex> lock(mutex);
+        thread_local int thread_id = global_id++;
+        return thread_id;
+    }
+
+    void print(std::string const &text)
+    {
+        static std::mutex mutex;
+        const std::lock_guard<std::mutex> lock(mutex);
+        std::cout << get_id() << " " << text << "\n";
+    }
+
     void async_run(std::launch launch_option, std::string message, int counter)
     {
     }
