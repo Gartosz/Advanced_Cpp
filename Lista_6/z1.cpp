932db0d083516a4ca0d485180774b3e1d8721035
Bartosz <bartekg001@gmail.com>
2022-12-15 16:30:23 +0100

    Create base file.

---
+++ Lista_6/z1.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <future>
+
+namespace cpplab
+{
+    void dot_product(std::vector<double> vec_1, std::vector<double> vec_2, std::promise<double>)
+    {
+
+    }
+}
+
+int main()
+{
+
+}
