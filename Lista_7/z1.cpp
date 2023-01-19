584c9c5444069a49d801e464bd142d6f0bb45aa2
Bartosz <bartekg001@gmail.com>
2023-01-19 16:22:37 +0100

    Add vector of tasks.

--- Lista_7/z1.cpp
+++ Lista_7/z1.cpp
@@ -1,11 +1,18 @@
+#include <vector>
+#include <iostream>
+#include <functional>
+
 namespace cpplab
 {
     class ThreadPool
     {
+        public:
         ThreadPool()
         {
 
         }
+        private:
+        std::vector<std::function<double()>> task_vector;
     };
 }
 
