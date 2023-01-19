bfd01ddbbe508210ccf70527bdb297988697ae98
Bartosz <bartekg001@gmail.com>
2023-01-20 00:50:46 +0100

    Basic constructor.

--- Lista_7/z1.cpp
+++ Lista_7/z1.cpp
@@ -8,9 +8,10 @@ namespace cpplab
     class ThreadPool
     {
         public:
-        ThreadPool()
+        ThreadPool(size_t threads_count)
         {
-            
+            for (int i = 0; i < threads_count; ++i)
+                threads.emplace_back(std::thread());
         }
 
         void add_task(std::function<double()> task)
