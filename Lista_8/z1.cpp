87062f6577452abeb591f02e66d5d4a7aa9995e8
Bartosz <bartekg001@gmail.com>
2023-02-02 16:07:58 +0100

    FuelTank constructor.

--- Lista_8/z1.cpp
+++ Lista_8/z1.cpp
@@ -2,7 +2,11 @@ namespace cpplab
 {
     class FuelTank
     {
-        
+        unsigned int fuel_level = 0;
+        FuelTank(unsigned int fuel)
+        {
+            fuel_level = fuel;
+        }
     };
 
     class Engine
