f89e2c17387091ac621e83d425795abb60080bab
Bartosz <bartekg001@gmail.com>
2023-02-02 16:10:33 +0100

    Implement refuel method.

--- Lista_8/z1.cpp
+++ Lista_8/z1.cpp
@@ -3,9 +3,17 @@ namespace cpplab
     class FuelTank
     {
         unsigned int fuel_level = 0;
-        FuelTank(unsigned int fuel)
+        FuelTank(unsigned int fuel_base)
         {
-            fuel_level = fuel;
+            fuel_level = fuel_base;
+        }
+
+        unsigned int refuel(unsigned int requested_fuel)
+        {
+            if (requested_fuel > fuel_level)
+                return 0;
+            fuel_level -= requested_fuel;
+            return requested_fuel;
         }
     };
 
