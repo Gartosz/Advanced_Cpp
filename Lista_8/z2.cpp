95eb1fab90808c2223f74345dd8ef496ec31f387
Bartosz <bartekg001@gmail.com>
2023-02-08 23:07:27 +0100

    Create unique_ptr class with default constructor and pointer member.

--- Lista_8/z2.cpp
+++ Lista_8/z2.cpp
@@ -1,6 +1,12 @@
 namespace cpplab
 {
-
+    template <typename T>
+    class unique_ptr
+    {
+        unique_ptr() : pointer(nullptr){} 
+        private:
+        T* pointer;
+    };
 }
 
 int main()
